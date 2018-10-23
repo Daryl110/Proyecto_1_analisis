8dc21ea351701df64142f1dff39a29c02775cc3c
Daryl110 <daryl.8220@gmail.com>
2018-10-23 09:09:23 -0500

    reportes echos

--- src/main/java/com/eam/analisis/controlador/reportes/CtlReportes.java
+++ src/main/java/com/eam/analisis/controlador/reportes/CtlReportes.java
@@ -6,6 +6,8 @@ package com.eam.analisis.controlador.reportes;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -19,12 +21,14 @@ import net.sf.jasperreports.view.JasperViewer;
  */
 public class CtlReportes {
     
-    public static void graficarOrdenamientos(){
+    public static void graficarOrdenamientos(int limit){
+        Map<String, Object> parametros = new HashMap<>();
+        parametros.put("numCanciones", limit);
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             Connection cone = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "Analisis", "1094971007");
             JasperReport jr = JasperCompileManager.compileReport("./src/main/resources/Reportes/Estadisticas_ordenacion.jrxml");
-            JasperPrint jp = JasperFillManager.fillReport(jr, null, cone);
+            JasperPrint jp = JasperFillManager.fillReport(jr, parametros, cone);
             JasperViewer jv = new JasperViewer(jp, false);
             jv.setLocationRelativeTo(null);
             jv.setVisible(true);
